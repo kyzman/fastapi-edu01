c28c952cc929e20fbf4dd648f716b14d1139ca7f
Andrew <kyzwork@yandex.ru>
2023-12-17 16:00:57 +0500

    4th step. Get post by id and latest

--- socialmedia/main.py
+++ socialmedia/main.py
@@ -19,6 +19,12 @@ my_posts = [{"title": "title of post1", "content": "content of post1", "id": 1},
             {"title": "title of post3", "content": "content of post3", "id": 3}]
 
 
+def find_post(id):
+    for p in my_posts:
+        if id == p['id']:
+            return p
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -34,4 +40,17 @@ def create_posts(new_post: Post):
     post = dict(new_post)
     post['id'] = randrange(0,1000000)
     my_posts.append(post)
-    return {"new_post": f"new post received", "data" : post}
+    return {"new_post": f"new post received", "data": post}
+
+
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts)-1]
+    return post
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"post_detail": post}
+
