bc5c0768e39ffd3cbabbce17bc75348edf9dbbf0
Andrew <kyzwork@yandex.ru>
2023-12-17 13:21:50 +0500

    3rd step. Create simple DB in memory. Get and put posts in that DB.

--- socialmedia/main.py
+++ socialmedia/main.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
-
+from random import randrange
 
 app = FastAPI()
 
@@ -14,6 +14,11 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [{"title": "title of post1", "content": "content of post1", "id": 1},
+            {"title": "favorite food", "content": "I like pizza", "id": 2},
+            {"title": "title of post3", "content": "content of post3", "id": 3}]
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -21,11 +26,12 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(new_post: Post):
-    print(new_post)
-    print(dict(new_post))
-    return {"new_post": f"new post received"}
+    post = dict(new_post)
+    post['id'] = randrange(0,1000000)
+    my_posts.append(post)
+    return {"new_post": f"new post received", "data" : post}
