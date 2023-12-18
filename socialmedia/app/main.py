c0bf673f17378a31e474c23e9fb0420d81aa1159
Andrew <kyzwork@yandex.ru>
2023-12-18 14:08:31 +0500

    10th step. Prepare code for refactor to work with DB using SQLAlchemy. Create engine and model

--- socialmedia/app/main.py
+++ socialmedia/app/main.py
@@ -1,11 +1,18 @@
 from typing import Optional
 
-from fastapi import FastAPI, Body, Response, status, HTTPException
+from fastapi import FastAPI, Body, Response, status, HTTPException, Depends
 from pydantic import BaseModel
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+
+from sqlalchemy.orm import Session
+
+from app import models
+from app.database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -32,6 +39,11 @@ while True:
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status": "success"}
 
 
 @app.get("/posts")
