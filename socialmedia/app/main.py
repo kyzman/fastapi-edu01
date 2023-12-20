1715ae365301a999cb754a638abbf2aa4a182278
Andrew <kyzwork@yandex.ru>
2023-12-20 14:15:13 +0500

    16th step. Create simple get user info

--- socialmedia/app/main.py
+++ socialmedia/app/main.py
@@ -2,12 +2,10 @@ from typing import List
 
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
 
-from app import models, schemas
+from app import models, schemas, utils
 from app.database import engine, get_db
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -18,7 +16,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/posts",response_model=List[schemas.Post])
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
@@ -71,8 +69,19 @@ def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(ge
 
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    user.password = utils.hash_pwd(user.password)
     new_user = models.User(**dict(user))  # create entry
     db.add(new_user)  # add entry to DB
     db.commit()  # commit changes
     db.refresh(new_user)   # RETURNING *
     return new_user
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserOut)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id: {user_id} was not found!")
+
+    return user
