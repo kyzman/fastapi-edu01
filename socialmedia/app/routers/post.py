41e6f8fafc064a9aa816b7bd16d71208fd24b9a1
Andrew <kyzwork@yandex.ru>
2023-12-23 14:28:51 +0500

    21st step. Add url vars to posts. Pagination & simple title search

--- socialmedia/app/routers/post.py
+++ socialmedia/app/routers/post.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 
@@ -15,8 +15,9 @@ router = APIRouter(
 
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db),
-              current_user: int = Depends(oauth2.get_current_user)):
-    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+              current_user: int = Depends(oauth2.get_current_user),
+              limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).offset(skip).limit(limit).all()
     return posts
 
 
