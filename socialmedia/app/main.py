b9966faca13b20f070ba28aeca5aa67e132c2ec7
Andrew <kyzwork@yandex.ru>
2023-12-24 19:54:13 +0500

    25th step. Prepare for first deploy

--- socialmedia/app/main.py
+++ socialmedia/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.post import router as post_router
 from app.routers.user import router as user_router
@@ -6,6 +7,16 @@ from app.routers.auth import router as auth_router
 from app.routers.vote import router as vote_router
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post_router)
 app.include_router(user_router)
