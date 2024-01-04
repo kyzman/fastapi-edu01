ed2902b790092d55a7a5139641edd94b2f8d649b
Andrew <kyzwork@yandex.ru>
2024-01-04 13:44:37 +0500

    #2.1 simple test create user

--- socialmedia/tests/test_users.py
+++ socialmedia/tests/test_users.py
@@ -1,12 +1,19 @@
 from fastapi.testclient import TestClient
 from app.main import app
-
+from app import schemas
 
 client = TestClient(app)
 
 
 def test_root():
     res = client.get("/")
-    print(res.json())
+    print("\n", res.json())
     assert res.json().get('message') == "Hello World!"
     assert res.status_code == 200
+
+
+def test_create_user():
+    res = client.post("/users/", json={"email": "hello123@gmail.com", "password": "password123"})
+    print("\n", res.json())
+    assert res.json().get('email') == "hello123@gmail.com"
+    assert res.status_code == 201
