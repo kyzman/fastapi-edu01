34d33dac52e317b8a5c6e1832a61e5e23b782de5
Andrew <kyzwork@yandex.ru>
2024-01-04 22:22:46 +0500

    #7 test. Makes login with custom user using fixture instead user creation test. Extended login test

--- socialmedia/tests/database.py
+++ socialmedia/tests/database.py
@@ -19,7 +19,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -30,9 +30,8 @@ def session():
         db.close()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture
 def client(session):
-
     def override_get_db():
         try:
             yield session
--- socialmedia/tests/test_users.py
+++ socialmedia/tests/test_users.py
@@ -1,12 +1,26 @@
+import pytest
+from jose import jwt
+
 from app import schemas
+from app.config import settings
 from tests.database import client, session
 
 
-def test_root(client):
-    res = client.get("/")
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "testmail@mail.ru", "password": "pass123"}
+    res = client.post("/users/", json=user_data)
 
-    assert res.json().get('message') == "Hello World!"
-    assert res.status_code == 200
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+# def test_root(client):
+#     res = client.get("/")
+#
+#     assert res.json().get('message') == "Hello World!"
+#     assert res.status_code == 200
 
 
 def test_create_user(client):
@@ -17,8 +31,12 @@ def test_create_user(client):
     assert res.status_code == 201
 
 
-def test_login_user(client):
-    res = client.post("/login", data={"username": "hello123@gmail.com", "password": "password123"})
-    print(res.json())
+def test_login_user(test_user, client):
+    res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    uid = payload.get("user_id")
+    assert uid == test_user['id']
+    assert login_res.token_type == 'bearer'
     assert res.status_code == 200
 
