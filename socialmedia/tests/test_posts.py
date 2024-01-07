3e04c24663b0bbc159d0c0eb209540b1a05920fc
Andrew <kyzwork@yandex.ru>
2024-01-07 23:06:09 +0500

    #10 test. Test unauthorized access to posts

--- socialmedia/tests/test_posts.py
+++ socialmedia/tests/test_posts.py
@@ -8,8 +8,19 @@ def test_get_all_posts(authorized_client, test_posts):
     def validate(post):
         return schemas.PostOut(**post)
 
-
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
+
+
+def test_unauthorized_get_all_posts(client):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+
+def test_unauthorized_get_one_posts(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
 
 
