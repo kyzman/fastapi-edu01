74049a0490824f99c9a9c8b261252607ca971b05
Andrew <kyzwork@yandex.ru>
2024-01-07 22:29:48 +0500

    #9.1 test. Simple hardcode test posts

--- socialmedia/tests/conftest.py
+++ socialmedia/tests/conftest.py
@@ -9,6 +9,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.config import settings
+from app.oauth2 import create_access_token
+
+from app import models
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:loop@localhost:5432/socialmedia_test"
 SQLALCHEMY_DATABASE_URL = (f'postgresql://{settings.database_username}:{settings.database_password}@'
@@ -42,6 +45,7 @@ def client(session):
 
     yield TestClient(app)
 
+
 @pytest.fixture
 def test_user(client):
     user_data = {"email": "testmail@mail.ru", "password": "pass123"}
@@ -51,3 +55,48 @@ def test_user(client):
     new_user = res.json()
     new_user['password'] = user_data['password']
     return new_user
+
+
+@pytest.fixture
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+
+    return client
+
+
+@pytest.fixture
+def test_posts(test_user, session):
+    posts_data = [
+        {"title": "1st title",
+         "content": "content for 1st post",
+         "owner_id": test_user['id']},
+        {"title": "2nd title",
+         "content": "content for 2nd post",
+         "owner_id": test_user['id']},
+        {"title": "3rd title",
+         "content": "content for 3rd post",
+         "owner_id": test_user['id']}
+    ]
+    session.add_all([
+        models.Post(title="1st title",
+                    content="content for 1st post",
+                    owner_id=test_user['id']),
+        models.Post(title="2nd title",
+                    content="content for 2nd post",
+                    owner_id=test_user['id']),
+        models.Post(title="3rd title",
+                    content="content for 3rd post",
+                    owner_id=test_user['id'])
+    ])
+
+    session.commit()
+
+    session.query(models.Post).all()
