c86261a0858d6c7df2e789e0c2e44616318ed3b2
Andrew <kyzwork@yandex.ru>
2024-01-07 23:22:47 +0500

    #11 test. Test one exist post and not exist

--- socialmedia/tests/test_posts.py
+++ socialmedia/tests/test_posts.py
@@ -22,5 +22,18 @@ def test_unauthorized_get_one_posts(client, test_posts):
     assert res.status_code == 401
 
 
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[1].id}")
+    post = schemas.PostOut(**res.json())
+
+    assert post.Post.id == test_posts[1].id
+    assert post.Post.content == test_posts[1].content
+    assert post.Post.title == test_posts[1].title
+    assert res.status_code == 200
+
+
+def test_one_post_not_exists(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/888")
+    assert res.status_code == 404
 
 
