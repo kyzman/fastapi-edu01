afb68e8b1373c6c545dcd259cf864ee6e15f4ab9
Andrew <kyzwork@yandex.ru>
2024-01-08 21:08:28 +0500

    #15 test. Test votes

--- socialmedia/app/routers/vote.py
+++ socialmedia/app/routers/vote.py
@@ -21,7 +21,7 @@ def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = 
     query = db.query(models.Vote).filter(
         (models.Vote.post_id == vote.post_id) & (models.Vote.user_id == current_user.id))
     voted = query.first()
-    print(voted)
+
     if (vote.direction == 1):
         if voted:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user {current_user.id} already voted on post {vote.post_id}")
---
+++ socialmedia/tests/test_votes.py
@@ -0,0 +1,46 @@
+import pytest
+from app import models
+
+
+@pytest.fixture
+def test_vote(test_posts, session, test_user):
+    new_vote = models.Vote(post_id=test_posts[3].id, user_id=test_user['id'])
+    session.add(new_vote)
+    session.commit()
+
+
+def test_vote_on_post(authorized_client, test_posts):
+    res = authorized_client.post("/vote/", json={"post_id": test_posts[0].id, "direction": 1})
+
+    assert res.status_code == 201
+
+
+def test_vote_twice_post(authorized_client, test_posts, test_vote):
+    res = authorized_client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 1})
+
+    assert res.status_code == 409
+
+
+def test_vote_delete(authorized_client, test_posts, test_vote):
+    res = authorized_client.post("/vote/", json={"post_id": test_posts[3].id, "direction": 0})
+
+    assert res.status_code == 201
+
+
+def test_vote_delete_not_exists(authorized_client, test_posts):
+    res = authorized_client.post("/vote/", json={"post_id": test_posts[1].id, "direction": 0})
+
+    assert res.status_code == 404
+
+
+def test_vote_on_post_not_exists(authorized_client, test_posts):
+    res = authorized_client.post("/vote/", json={"post_id": 888, "direction": 1})
+
+    assert res.status_code == 404
+
+
+def test_vote_unauthorized(client, test_posts):
+    res = client.post("/vote/", json={"post_id": test_posts[0].id, "direction": 1})
+
+    assert res.status_code == 401
+
