0851da6f841f9b794eb6d96fc97cc7b5296766da
Andrew <kyzwork@yandex.ru>
2024-01-08 14:29:52 +0500

    #12 test. Test create posts

--- socialmedia/tests/test_posts.py
+++ socialmedia/tests/test_posts.py
@@ -1,4 +1,4 @@
-from typing import List
+import pytest
 from app import schemas
 
 
@@ -37,3 +37,33 @@ def test_one_post_not_exists(authorized_client, test_posts):
     assert res.status_code == 404
 
 
+@pytest.mark.parametrize("title, content, published", [
+    ("awesome new title", "awesom new content", True),
+    ("favorite pizza", "I live pepperoni", False),
+    ("tallest skyscrapers", "wahoo", True),
+])
+def test_create_post(authorized_client, test_user, test_posts, title, content, published):
+    res = authorized_client.post("/posts/", json={"title": title, "content": content, "published": published})
+    new_post = schemas.Post(**res.json())
+
+    assert res.status_code == 201
+    assert new_post.title == title
+    assert new_post.content == content
+    assert new_post.published == published
+    assert new_post.owner_id == test_user['id']
+
+
+def test_create_post_default_published_true(authorized_client, test_user, test_posts):
+    res = authorized_client.post("/posts/", json={"title": "Arbitrary title", "content": "some content"})
+    new_post = schemas.Post(**res.json())
+
+    assert res.status_code == 201
+    assert new_post.title == "Arbitrary title"
+    assert new_post.content == "some content"
+    assert new_post.published == True
+    assert new_post.owner_id == test_user['id']
+
+
+def test_unauthorized_create_post(client):
+    res = client.post(f"/posts/", json={"title": "Dummy title", "content": "some dummy content"})
+    assert res.status_code == 401
